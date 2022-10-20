9f6ad6bd720e07f6fb9cc1b2b3d9dac1d4a1d13c
Đặng Phúc Minh <0baogiohet39@gmail.com>
2022-10-20 11:51:39 +0700

    first1

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 // This tells Express we’re using EJS as the template engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 
 app.get("/add-products", (req, res) => { 
     res.render("addProduct"); 
@@ -23,7 +21,6 @@ const connectionString = 'mongodb+srv://dpminh:Abcd1234@cluster0.h8fy9pi.mongodb
 MongoClient.connect(connectionString, (err, client) => {
 
     if (err) return console.error(err)
-
     console.log('Connected to Database')
 
     // server -> create -> database -> 'star-wars-quotes' 
@@ -41,11 +38,9 @@ MongoClient.connect(connectionString, (err, client) => {
 
                 // server -> result -> console 
                 console.log(result)
-
                 res.redirect('/')
             })
             .catch(error => console.error(error))
-
     })
 
     // We normally abbreviate `request` to `req` and `response` to `res`.
@@ -54,20 +49,14 @@ MongoClient.connect(connectionString, (err, client) => {
     app.get('/', (req, res) => {
         db.collection('quotes').find().toArray()
             .then(results => {
-
-                //console.log(results)
                 // server -> index.ejs -> client 
                 res.render('index.ejs', { quotes: results })
-
             })
             .catch(error => console.error(error))
-
     })
-
 
     // server -> listen -> port -> PORT
     app.listen(PORT, function () {
         console.log('listening on ' + PORT)
     })
-
 })
